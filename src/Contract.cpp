0bafe80aa0b2cadf92c7f019a6b4b7de925854bb
Imagic <imagicthecat@gmail.com>
2018-04-21 12:50:10 +0200

    Begin contract implementation.

---
+++ src/Contract.cpp
@@ -0,0 +1,90 @@
+
+#include "Contract.hpp"
+
+void Contract::load(const std::string &data)
+{
+  // init
+  content = data;
+  data_steps.clear();
+
+  // parse
+  size_t lpos = 0; // begin cursor
+  size_t spos; // end cursor
+  while((spos = content.find("\r\n=", lpos)) != std::string::npos){
+    std::string data_step(content.substr(lpos, spos+2));
+
+    lpos = spos+3;
+
+    // find signature
+    size_t end_sign = content.find("\r\n", lpos);
+    if(end_sign != std::string::npos){
+      std::string data_sign(hex2buf(content.substr(lpos, end_sign)));
+
+      // add data
+      data_steps.push_back(MapData());
+      MapData& mdata = data_steps[data_steps.size()-1];
+      mdata.load(data_step);
+
+      // add signature
+      signatures.push_back(std::pair<size_t, std::string>(spos+2, data_sign));
+
+      // next step
+      lpos = end_sign+2;
+    }
+  }
+}
+
+bool Contract::verify() const
+{
+  for(size_t i = 0; i < signatures.size(); i++){
+    const size_t& end = signatures[i].first;
+    const std::string& sign = signatures[i].second;
+    if(i <= data_steps.size()){
+      const MapData& data_step = data_steps[i];
+
+      std::string public_key;
+
+      if(data_step.has("public_key"))
+        public_key = hex2buf(data_step.get("public_key"));
+      else if(i == signatures.size()-1){ // last step without public key, find identity
+        if(data_steps[0].has("identity")) // identity in first contract step
+          public_key = hex2buf(data_steps[0].get("identity"));
+        else if(data_step.has("identity")) // identity last contract step
+          public_key = hex2buf(data_step.get("identity"));
+      }
+
+      if(sign.size() == 64 && public_key.size() == 32){
+        if(ed25519_verify((const unsigned char*)sign.c_str(), (const unsigned char*)content.c_str(), end, (const unsigned char*)public_key.c_str()) != 1)
+          return false;
+      }
+      else
+        return false;
+    }
+    else
+      return false;
+  }
+
+  return true;
+}
+
+
+bool Contract::sign(const std::string& public_key, const std::string& private_key)
+{
+  if(data_steps.size() > signatures.size() && public_key.size() == 32 && private_key.size() == 64){
+    // add step data to content
+    MapData& data_step = data_steps[data_steps.size()-1];
+    std::string data;
+    data_step.write(data);
+    content += data;
+
+    // sign content
+    unsigned char signature[64];
+    ed25519_sign(signature, (const unsigned char*)content.c_str(), content.size(), (const unsigned char*)public_key.c_str(), (const unsigned char*)private_key.c_str());
+
+    // add signature
+    content += "="+buf2hex((const char*)signature, 64)+"\r\n";
+    return true;
+  }
+
+  return false;
+}
---
+++ src/Contract.hpp
@@ -0,0 +1,39 @@
+
+#ifndef H_CONTRACT
+#define H_CONTRACT
+
+#include "tools.hpp"
+#include "MapData.hpp"
+#include "../sub/ed25519/src/ed25519.h"
+
+// load, add data, sign, write contract
+class Contract{
+  public:
+    void load(const std::string &data);
+    void write(std::string &data){ data = content; }
+
+    // verify the entire contract
+    bool verify() const;
+
+    // add a step if the previous one is already signed (or return the last)
+    MapData& next()
+    {
+      if(data_steps.size() == signatures.size())
+        data_steps.push_back(MapData());
+      return data_steps[data_steps.size()-1];
+    }
+
+    // sign the last added step, update contract content
+    // return true on success (meaning the content is modified, a post-verification is recommended)
+    bool sign(const std::string& public_key, const std::string& private_key);
+
+    const std::vector<std::pair<size_t, std::string> >& getSignatures()const{ return signatures; }
+    const std::vector<MapData>& getDataSteps()const{ return data_steps; }
+
+  private:
+    std::string content;
+    std::vector<MapData> data_steps;
+    std::vector<std::pair<size_t, std::string> > signatures; // start pos / buf signature
+};
+
+#endif
